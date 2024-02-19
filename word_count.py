c8b2fb929259c988cf8f97cc86e7182ccb5e3df9
sara-restrepo13 <sarestrepoo@unal.edu.co>
2024-02-19 10:03:03 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -13,20 +13,38 @@ def load_input(input_directory):
     # entrada en el DataFrame.
     #
 
+    filenames=glob.glob(input_directory+'/*.*')
+    dataframes=[pd.read_csv(filename,sep=";",names=['text']) for filename in filenames] #No tengo columna con nombre de las filas
+    dataframe=pd.concat(dataframes).reset_index(drop=True)  #Varios concat en una lista y los pega todos uno debajo del otro
+    return dataframe
+
 
 def clean_text(dataframe):
     """Text cleaning"""
     #
-    # Elimine la puntuación y convierta el texto a minúsculas.
+    # Elimine la puntuación y convierta el texto a minúsculas. - Primero hacer copia para poder modificar con tranquilidad
     #
+    dataframe=dataframe.copy()
+    dataframe['text']=dataframe['text'].str.lower()
+    dataframe['text']=dataframe['text'].str.replace(",","").str.replace(".","")
+    return dataframe
 
 
 def count_words(dataframe):
     """Word count"""
+    dataframe=dataframe.copy()
+    dataframe['text']=dataframe['text'].str.split() #Crear una lista de palabras
+    dataframe=dataframe.explode('text').reset_index(drop=True) #Trasponer, del ancho al largo
+    dataframe=dataframe.rename(columns={'text':'word'})
+    dataframe['count']=1
+
+    conteo=dataframe.groupby(['word'], as_index=False).agg({'count':sum}) #Fn generica de pandas que me dice como agrego las columnas
+    return conteo
 
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
+    dataframe.to_csv(output_filename, index=False, sep="\t")
 
 
 #
@@ -34,6 +52,13 @@ def save_output(dataframe, output_filename):
 #
 def run(input_directory, output_filename):
     """Call all functions."""
+
+    dataframe=load_input(input_directory)
+    dataframe=clean_text(dataframe)
+    dataframe=count_words(dataframe)
+    save_output(dataframe,output_filename)
+
+    
 
 
 if __name__ == "__main__":
